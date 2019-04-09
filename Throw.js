444de0f7563aa415b17c342fa5ea04fa7a603310
Pawan_Namagiri <pavann.namagiri@gmail.com>
2019-04-09 17:58:25 +0530

    added Throw.jsp

---
+++ Throw.js
@@ -0,0 +1,13 @@
+function isPositive(a) {
+
+    if (a == 0) {
+
+        throw new Error("Zero Error");
+    }
+    if (a > 0) {
+        return "YES";
+    }
+    if (a < 0) {
+        throw new Error("Negative Error");
+    }
+}
