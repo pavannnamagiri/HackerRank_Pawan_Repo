6a948b9a79b0268b5370ea68dba0e1b963d68168
Pawan Namagiri <pavann.namagiri@gmail.com>
2019-04-24 13:38:52 +0530

    added Anagram.js

---
+++ Anagram.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the anagram function below.
+function anagram(s) {
+
+    if (s.length % 2 != 0) {
+        return -1;
+    }
+    var sub1 = s.substring(0, s.length/2);
+    var sub2 = s.substring(s.length / 2);
+    var count = 0;
+
+    var arr1 = sub1.split("");
+    var arr2 = sub2.split("");
+
+    for (var i = 0; i < arr1.length; i++) {
+        for (var j = 0; j < arr2.length; j++) {
+            if (arr1[i] == arr2[j]) {
+                count++;
+                arr2[j] = "";
+                break;
+                
+            }
+        }
+    }
+
+    return sub1.length-count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        let result = anagram(s);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
